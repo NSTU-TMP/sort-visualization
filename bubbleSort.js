d43752e184f3d5a8b201e071ecadadccb798503a
xpomin <xpomin@pop-os.localdomain>
2023-05-15 18:47:21 +0700

    add sortingCanvas class

---
+++ bubbleSort.js
@@ -0,0 +1,24 @@
+window["bubbleSort"] = async function bubbleSort(values, drawingCanwas, drawValuesFunc, sleepTimeMS) {
+
+    let sleepTime = 0;
+    if (typeof (sleepTimeMS) != "undefined") {
+        sleepTime = sleepTimeMS;
+    }
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < values.length - i - 1; j++) {
+            let a = values[j];
+            let b = values[j + 1];
+            if (values[j] > values[j + 1]) {
+                let tmp = values[j];
+                values[j] = values[j + 1];
+                values[j + 1] = tmp;
+                drawValuesFunc(values, drawingCanwas, j, j + 1);
+                await sleep(sleepTime);
+            }
+        }
+
+    }
+};
+
+
+
--- script.js
+++ script.js
@@ -1,65 +1,97 @@
-let values = [];
-let i = 0;
-let j = 0;
-let isSorting = false;
+class SortingCanvas {
+    #canvas;
+    #sort_and_draw;
+    #values;
+    #draw_array_func_reference
+    #parent_id
+
+    constructor(width, height, parent_id, sort_function_name, values_array) {
+        this.#parent_id = parent_id;
+        this.#canvas = createCanvas(width, height);
+        this.#canvas.parent(parent_id);
+        this.#sort_and_draw = sort_function_name;
+        this.#values = values_array;
+        this.#draw_array_func_reference = SortingCanvas.drawValues;
+        this.reset();
+    }
+
+    getParentId() { return this.#parent_id; }
+
+    static drawValues(array, canvas, red_value_index_1, red_value_index_2) {
+        background(0);
+        for (let i = 0; i < array.length; i++) {
+            stroke(255);
+            if (i == red_value_index_1 || i == red_value_index_2) {
+                stroke(255, 0, 0);
+            }
+            canvas.line(i * 8, canvas.height, i * 8, canvas.height - array[i]);
+        }
+    }
+    sortAndDraw() {
+        window[this.#sort_and_draw](this.#values, this.#canvas, this.#draw_array_func_reference);
+        SortingCanvas.drawValues(this.#values, this.#canvas);
+
+    }
+
+    reset() {
+        if (typeof (this.#values) == "undefined") {
+            this.#values = generateArray(this.#canvas.width * 0.12);
+        } else {
+            this.#values = generateArray(this.#values.length);
+        }
+        SortingCanvas.drawValues(this.#values, this.#canvas);
+    }
+}
+
+let canvasArray = [];
+let isSorting = [];
 
 function setup() {
-    // const canvas = createCanvas(400, 400);
-    const canvas = createCanvas(window.innerWidth, window.innerHeight);
+    const ids = Array
+        .from(document.querySelectorAll('.sortVisualizer'))
+        .map(element => element.id);
 
-    canvas.parent('bubbleSortCanvas');
-    resetArray();
+    for (let i = 0; i < ids.length; i++) {
+        canvasArray.push(new SortingCanvas(
+            window.innerWidth,
+            window.innerHeight,
+            ids[i],
+            ids[i],
+            generateArray(
+                floor(window.innerWidth * 0.12)
+            ))
+        );
+    }
 }
 
 function draw() {
-    background(0);
-    if (isSorting) {
-        bubbleSort();
-    }
-    for (let i = 0; i < values.length; i++) {
-        stroke(255);
-        line(i * 8, height, i * 8, height - values[i]);
-    }
 }
 
-function bubbleSort() {
-    if (i < values.length) {
-        for (let j = 0; j < values.length - i - 1; j++) {
-            let a = values[j];
-            let b = values[j + 1];
-            if (a > b) {
-                swap(values, j, j + 1);
-            }
+function findCanvasIndexByParentId(canvas_parent_id) {
+    for (let i = 0; i < canvasArray.length; i++) {
+        if (canvasArray[i].getParentId() == canvas_parent_id) {
+            return i;
         }
-    } else {
-        console.log("Finished");
-        noLoop();
     }
-    i++;
 }
 
-function swap(arr, a, b) {
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+function startSortVisualization(canvas_parent_id) {
+    canvasArray[this.findCanvasIndexByParentId(canvas_parent_id)].sortAndDraw();
 }
 
-function startVisualization() {
-    isSorting = true;
+function resetSortVisualization(canvas_parent_id) {
+    canvasArray[this.findCanvasIndexByParentId(canvas_parent_id)].reset();
 }
 
-function resetVisualization() {
-    isSorting = false;
-    resetArray();
-    loop();
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function resetArray() {
+
+function generateArray(size) {
     values = [];
-    for (let i = 0; i < width / 8; i++) {
+    for (let i = 0; i < size; i++) {
         values.push(random(height));
     }
-    i = 0;
+    return values;
 }
-
-window.addEventListener('load', setup);
